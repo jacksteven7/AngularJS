853ebf52fc8dc3397cdbd48e18946afdbcc84bd7
jacksteven7 <jacksteven7@gmail.com>
2015-08-18 23:56:30 -0500

    Angular Forms

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
  angular.module('miAp', []);
 
-angular.module('miAp')
-.factory('Dato', function() {
+angular.module('miAp').factory('Dato', function() {
   return {mensaje: 'Saludos desde la Fabrica!'};
 });
 
-angular.module('miAp')
-.controller('ControladorUno', function($scope, Dato) {
+angular.module('miAp').controller('ControladorUno', function($scope, Dato) {
   $scope.dato = Dato;
 });
 
-angular.module('miAp')
-.controller('ControladorDos', function($scope, Dato) {
+angular.module('miAp').controller('ControladorDos', function($scope, Dato) {
   $scope.dato = Dato;
 });
