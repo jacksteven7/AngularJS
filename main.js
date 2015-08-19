497e0dba5fb4c6c6b318a0160a18caeea165784d
jacksteven7 <jacksteven7@gmail.com>
2015-08-18 22:52:49 -0500

    Factory Example in Angular

---
+++ main.js
@@ -0,0 +1,16 @@
+ angular.module('miAp', []);
+
+angular.module('miAp')
+.factory('Dato', function() {
+  return {mensaje: 'Saludos desde la Fabrica!'};
+});
+
+angular.module('miAp')
+.controller('ControladorUno', function($scope, Dato) {
+  $scope.dato = Dato;
+});
+
+angular.module('miAp')
+.controller('ControladorDos', function($scope, Dato) {
+  $scope.dato = Dato;
+});
